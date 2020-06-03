a58bf64f931ef2df5daf3861e62163b51cf46fef
moneyan9 <m.tanimura1226@gmail.com>
2020-06-03 19:41:56 +0900

    create guntt-chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GunttChartComponent } from './guntt-chart/guntt-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GunttChartComponent
   ],
   imports: [
     BrowserModule,
